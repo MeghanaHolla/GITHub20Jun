59819ac9f1e6cac1df7bfca8e0dde3d045f89d17
MEGHANA <MEGHANA@LAPTOP-95NO9S2C>
2021-06-20 21:02:26 +0530

    Saving Framework project on GIT

---
+++ Frameworks/src/com/webappsecurity/zero/TestScripts/Base.java
@@ -0,0 +1,33 @@
+package com.webappsecurity.zero.TestScripts;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class Base {
+
+	WebDriver driver;
+	@BeforeTest
+	public void openApplication() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		ChromeOptions capability = new ChromeOptions();
+		capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		capability.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS,true);
+
+		driver = new ChromeDriver(capability);
+		
+		driver.get("http://zero.webappsecurity.com/login.html");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+	
+	@AfterTest
+	public void closeApplication() {
+		driver.close();
+	}
+}
